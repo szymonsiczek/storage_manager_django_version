21eb570d82ff288f677dd87bad48dcdea10da7a9
Szymon Siczek <szymonsiczek@gmail.com>
2020-09-28 18:17:19 +0200

    add django choices and improved categories in show_item_from_category view

--- storagemanager/storage/models.py
+++ storagemanager/storage/models.py
@@ -2,7 +2,25 @@ from django.db import models
 
 
 class Item(models.Model):
-    category = models.CharField(max_length=100)
+    EXTRA = 'Extra'
+    LIGHT = 'Light'
+    POWER = 'Power'
+    SOUND = 'Sound'
+    STAGE = 'Stage'
+    
+    CATEGORIES = [
+        (EXTRA, 'Extra'),
+        (LIGHT, 'Light'),
+        (POWER, 'Power'),
+        (SOUND, 'Sound'),
+        (STAGE, 'Stage'),
+    ]
+    category = models.CharField(
+        max_length=10,
+        choices=CATEGORIES,
+        default=EXTRA,
+    )
+    # category = models.CharField(max_length=100)
     type = models.CharField(max_length=160)
     model = models.CharField(max_length=160)
     serial_number = models.CharField(max_length=20)
