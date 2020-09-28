00b738ecea4b17c7b1fdd18bf2bef18d66685700
Szymon Siczek <szymonsiczek@gmail.com>
2020-09-28 14:30:47 +0200

    made views working more with ORM

--- storagemanager/storage/views.py
+++ storagemanager/storage/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.core.exceptions import ObjectDoesNotExist
 from django.contrib import messages
 from django.views.generic import ListView, CreateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -22,7 +23,7 @@ class ShowAllListView(ListView):
     model = Item
     template_name = 'storage/show_all.html'
     context_object_name = 'all_items'
-    ordering = ['category', 'type']
+    ordering = ['category', 'type', 'model']
 
 def show_items_from_category(request):
     categories_set = set()                      
@@ -40,34 +41,47 @@ def delete_item(request):
     return render(request, 'storage/delete_item.html', context)
 
 def delete_item_confirm(request):
-    if request.POST.get('id') == 'Choose_item':
-        messages.warning(request, f'Please choose an item from the list.')
-        return redirect('delete-item')
-    try:
-        context = {
-            'item_to_delete': str(Item.objects.filter(id=request.POST.get('id')).first()), 
-            'id': request.POST.get('id')
-            }
-        if context.get('item_to_delete').startswith('None'):
-            messages.warning(request, f'Item with that ID could not be found.')
-            return redirect('delete-item')
-        else:
-            return render(request, 'storage/delete_item_confirm.html', context)
-    except ValueError:
+    if request.POST.get('id') == '':
         messages.warning(request, f'Please type a number')
         return redirect('delete-item')
+    elif 'e' in request.POST.get('id'):
+        messages.warning(request, f'Exponent numbers are not allowed')
+        return redirect('delete-item')
+    else:
+        item_id = request.POST.get('id')
+
+    if item_id == 'Choose_item':
+        messages.warning(request, f'Please choose an item from the list.')
+        return redirect('delete-item')
+
+    try:
+        item_to_delete = Item.objects.get(id=item_id)
+    except ObjectDoesNotExist:
+        item_to_delete = None
+    context = {
+        'item_to_delete': item_to_delete, 
+        'id': item_id
+        }
+    if context.get('item_to_delete') == None:
+        messages.warning(request, f'Item with that ID could not be found.')
+        return redirect('delete-item')
+    else:
+        return render(request, 'storage/delete_item_confirm.html', context)
+
 
 def delete_item_after_confirm(request):
-    item_var = Item.objects.filter(id=request.POST.get('id')).first()
-    Item.objects.filter(id=request.POST.get('id')).first().delete()
-    messages.success(request, f'Item: {item_var.type} {item_var.model}, SN: {item_var.serial_number} has been deleted')
+    item_to_delete = Item.objects.get(id=request.POST.get('id'))
+    item_to_delete_type = item_to_delete.type
+    item_to_delete_model = item_to_delete.model
+    item_to_delete_serial_number = item_to_delete.serial_number
+    item_to_delete.delete()
+    messages.success(request, f'Item: {item_to_delete_type} {item_to_delete_model}, SN: {item_to_delete_serial_number} has been deleted')
     return redirect('delete-item')
 
 def delete_all_items(request):
     if request.method == 'POST':
         if request.POST.get('delete_all_confirmation') == 'yes':
-            for item in Item.objects.all():
-                item.delete()
+            Item.objects.all().delete()
             messages.success(request, 'All items were deleted')
             return render(request, 'storage/delete_all_items.html')
     else:       
