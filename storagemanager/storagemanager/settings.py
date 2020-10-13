0c04ef718ae9e5234a604ad950cbe318adeacd27
Szymon Siczek <szymonsiczek@gmail.com>
2020-10-13 11:05:30 +0200

    b9 created user registration page

--- storagemanager/storagemanager/settings.py
+++ storagemanager/storagemanager/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'storage.apps.StorageConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
