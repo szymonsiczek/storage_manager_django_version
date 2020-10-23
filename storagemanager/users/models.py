b68d90ff626edf381a3c74ed5e7d095e0cf3ebae
Szymon Siczek <szymonsiczek@gmail.com>
2020-10-23 11:03:32 +0200

    b9 added RGBA=>RGB converting to profile model's image

--- storagemanager/users/models.py
+++ storagemanager/users/models.py
@@ -16,6 +16,8 @@ class Profile(models.Model):
         super().save(*args, **kwargs)
 
         img = Image.open(self.image.path)
+        if img.mode == 'RGBA':
+            img = img.convert('RGB')
 
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
